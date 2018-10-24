dc6f5dae4170235c13f0b3753df739e138711e75
katejsenger <katejsenger@yahoo.com>
2018-10-24 03:23:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ students = ["Todd", "Amy", "Jay", "Tom"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+students = ["Todd", "Amy", "Jay", "Tom"]
+students.push("Jim")
 end
 
 def add_element_to_start_of_array(array, element)
