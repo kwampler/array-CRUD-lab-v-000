5c0adb1ceedcb95ee5f8064c4175416118077dec
katejsenger <katejsenger@yahoo.com>
2018-10-24 03:38:54 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ array = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array.index("am")
+array.index("index_number")
 end
 
 def retrieve_first_element_from_array(array)
